12989bee727fd9395196fb9117686ff253be36ae
raiyhan17 <r_bhui@live.concordia.ca>
2023-06-12 14:47:41 -0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Form from './components/Form';
 import YupForm from './components/YupForm';
 import './App.css';
+//new Component
 
 function App() {
   return (
     <div>
-      {/* <Form/> */}
-      <YupForm/>
+      <Form/>
+      {/* <YupForm/> */}
     </div>
   );
 }
